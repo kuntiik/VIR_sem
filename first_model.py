946c6f9065216c3a56caee24183bf69e6218c35b
kuntluka <kuntluka@fel.cvut.cz>
2019-12-10 18:13:02 +0100

    changed loading

--- first_model.py
+++ first_model.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch import optim
 import torch.utils.data as tdata
 import os
+import torchvision
 
 path = "/local/temporary/audi/camera/"
 path_pic = "/local/temporary/audi/camera/camera/cam_front_center/"
@@ -12,13 +13,13 @@ path_labels = "labels/
 def load_data():
     pics = []
     labels = []
-    for name in sorted(os.listdir(path_pic)):
-        if name.endswith(".png"):
-            pics.append(open(path_pic + name, "rb"))
+    train_dataset = torchvision.ImageFolder(
+         root=path_pic, transform=torchvision.transforms.ToTensor())
 
     for name in sorted(os.listdir(path_labels)):
         f = open(path_labels + name, "rb")
         labels.append(json.load(f)['Angle'])
+        f.close()
 
 def main():
     load_data()
